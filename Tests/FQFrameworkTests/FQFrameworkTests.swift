f6cb998ca7b031b4eb77d23b25ae7558a5944e2e
Fede Q <fede.cugliandolo@gmail.com>
2020-05-20 19:48:15 -0300

    added new class and unit test

--- Tests/FQFrameworkTests/FQFrameworkTests.swift
+++ Tests/FQFrameworkTests/FQFrameworkTests.swift
@@ -1,12 +1,19 @@
 import XCTest
+import SwiftUI
 @testable import FQFramework
 
 final class FQFrameworkTests: XCTestCase {
+    @State var text: String
+    
+    init(text: String) {
+        self.text = text
+        super.init()
+    }
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FQFramework().text, "Hello, World!")
+        var body: some View {
+            FQFramework.MainTextField(placeholder: "Testing", text: $text)
+        }
     }
 
     static var allTests = [
