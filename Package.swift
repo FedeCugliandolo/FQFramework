9a1885e6b7e1a6af6efede55f8e71710e1c91315
Fede Q <fede.cugliandolo@gmail.com>
2020-05-20 19:25:16 -0300

    added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FQFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FQFramework",
+            type: .dynamic,
             targets: ["FQFramework"]),
     ],
     dependencies: [
